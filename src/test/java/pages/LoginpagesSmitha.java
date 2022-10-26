701503bdcd93a0a7329dbd7c34454cc2ba408603
smitha2509@gmail.com <smitha2509@gmail.com>
2022-10-26 23:29:42 +0100

    added login page

--- src/test/java/pages/LoginpagesSmitha.java
+++ src/test/java/pages/LoginpagesSmitha.java
@@ -1,22 +1,28 @@
-package src.test.java.pages;
+package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class Loginpages {
-
+public class LoginpagesSmitha {
     private WebDriver driver;
-
-
-
-    public Loginpages(WebDriver driver) {
+    public LoginpagesSmitha(WebDriver driver) {
         this.driver=driver;
         PageFactory.initElements(driver,this);
-
     }
+    @FindBy(id = "inputEmail")
+    private WebElement username;
+    @FindBy(id="inputPassword")
+    private WebElement password;
+    @FindBy(xpath = "//button[@type=\"submit\"]")
+    private WebElement submitBtn;
 
-
-
+    public void login(String arg1,String arg2){
+        username.sendKeys(arg1);
+    password.sendKeys(arg2);
+    submitBtn.click();
+}
 
 
 }
