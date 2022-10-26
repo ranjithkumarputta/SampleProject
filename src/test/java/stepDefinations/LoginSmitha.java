ef537c97dea1c88d88c263e9c3d5fe12bc56dac0
smitha2509@gmail.com <smitha2509@gmail.com>
2022-10-26 23:19:23 +0100

    adding files

--- src/test/java/stepDefinations/LoginSmitha.java
+++ src/test/java/stepDefinations/LoginSmitha.java
@@ -1,27 +1,33 @@
 package stepDefinations;
 
-import pages.Loginpages;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 
-public class Login  extends CommonFunction {
-    private Loginpages loginobj;
+import pages.LoginpagesSmitha;
+import stepDefinations.CommonFunctionSmitha;
+
+public class LoginSmitha extends CommonFunctionSmitha {
+    private LoginpagesSmitha loginobj;
+
+    public LoginSmitha(){
+        loginobj=new LoginpagesSmitha(driver);
+    }
     @Given("^user on IM login page$")
     public void user_on_IM_login_page() throws Throwable {
 
-        loginobj=new Loginpages(driver);
+        //loginobj=new LoginpagesSmitha(driver);
         driver.get("http://3.13.126.58/AMS3.0/auth/login");
 
     }
 
     @When("^user enters username \"([^\"]*)\" and password\"([^\"]*)\"$")
     public void user_enters_username_and_password(String arg1, String arg2) throws Throwable {
-        driver.findElement(By.id("inputEmail")).sendKeys(arg1);
-        driver.findElement(By.id("inputPassword")).sendKeys(arg2);
-        driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
-
+      //  driver.findElement(By.id("inputEmail")).sendKeys(arg1);
+       // driver.findElement(By.id("inputPassword")).sendKeys(arg2);
+       // driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+loginobj.login(arg1,arg2);
     }
 
     @Then("^user should able to login$")
